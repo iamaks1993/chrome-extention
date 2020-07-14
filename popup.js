fb378a481a78e2ae7e00e884cc74e11a15cde135
Akshay Mahajan <akshay.iihglobal@gmail.com>
2020-07-14 12:14:26 +0530

    Updated chrome extention

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    var txtInp = document.getElementById("txt_test_inp");
+    txtInp.value = localStorage.getItem('test-inp');
     var counter = 1;
     var checkPageButton = document.getElementById('checkPage');
+
     checkPageButton.addEventListener('click', function() {
 
-        alert(++counter);
+        localStorage.setItem('test-inp',txtInp.value);
+        //alert(counter++);
         // chrome.tabs.getSelected(null, function(tab) {
         //     d = document;
         //
@@ -19,4 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
         //     f.submit();
         // });
     }, false);
+
+  /*var dataToWebPage = {text: 'test', foo: 1, bar: false};
+    chrome.tabs.executeScript({
+        code: '(' + function(params) {
+            //This function will  work in webpage
+            console.log(params); //logs in webpage console
+            return {success: true, response: "This is from webpage."};
+        } + ')(' + JSON.stringify(dataToWebPage) + ');'
+    }, function(results) {
+        //This is the callback response from webpage
+        console.log(results[0]); //logs in extension console
+    });*/
 }, false);
